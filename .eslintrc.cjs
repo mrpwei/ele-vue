5a0b76bfa8846688528057ce793d3b5bfefc3c6e
pengwei <mrpwei@outlook.com>
2023-03-22 14:35:15 +0800

    feat: 初始化项目

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 100, // 单行代码字符数量不能超过100
+  tabWidth: 2,
+  useTabs: false, // 不使用制表符缩进，使用空格
+  semi: false, // 不需要分号结束
+  singleQuote: true, // 使用单引号
+  bracketSpacing: true, // 对象左右都需要空格
+}

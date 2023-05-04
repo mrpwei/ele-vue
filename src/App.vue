b2691cc6389af8f59c7d63353e2682ac311746be
pengwei <mrpwei@outlook.com>
2023-05-04 15:23:25 +0800

    feat: 配置axios请求

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+import { fetchTest } from '@/api/test'
+fetchTest()
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // +
+    alias: {
+      // +
+      '@': path.resolve(__dirname, 'src'), // +
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8000',
+      '/imgs': 'http://lcalhost:8000',
+    },
+  },
 })
